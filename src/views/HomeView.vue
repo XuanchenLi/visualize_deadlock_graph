3e34f3d44754b24f862f139128c8e0686bfb75a6
Dazz1e <2468691031@qq.com>
2022-09-10 12:02:33 +0800

    graph

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,6 +10,27 @@
     <el-container>
       <div class="graph" id="graph" style="margin-top:40px;width: 1600px;height:895px;" @contextmenu="openMenu" @dblclick="openTab">
       </div>
+      <div style="margin-top:40px; height: 895px; width: 330px">
+        <el-row style="margin-top: 5px">
+          <el-button type="primary" style="width: 60px; margin-left: 13px; padding-left: 18px">
+            GO
+          </el-button>
+          <el-button type="success" >
+            Clear
+          </el-button>
+          <el-button type="warning" style="width: 60px; padding-left: 15px">
+            Step
+          </el-button>
+          <el-button type="info" style="width: 70px; padding-left: 18px">
+            Back
+          </el-button>
+        </el-row>
+        <div class="chat" style="">
+          <ul style="overflow-y: scroll">
+
+          </ul>
+        </div>
+      </div>
     </el-container>
     <ul v-show="visible" :style="{left:left+'px',top:top+'px'}" class="contextmenu" @contextmenu="prevent">
       <!-- <li v-if="rightClickItem.fileType==99" @click="handleClickFolder(rightClickItem)">打开</li>
@@ -45,7 +66,7 @@
           />
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitForm(ruleFormRef)"
+          <el-button type="primary" @click="submitForm(ruleFormRef, $event)"
           >Submit</el-button
           >
         </el-form-item>
@@ -62,6 +83,7 @@ import {GraphEdgeItemOption, GraphNodeItemOption} from "echarts/types/src/chart/
 import {EChartsType} from "echarts/core";
 import {reactive, ref, watch} from "vue";
 import {ElFormItem} from "element-plus";
+import {Refresh} from '@element-plus/icons'
 
 
 type EchartsOption = echarts.EChartsOption
@@ -83,7 +105,7 @@ interface Node {
 
 @Options({
   components: {
-
+    Refresh
   },
 })
 export default class HomeView extends Vue {
@@ -243,17 +265,18 @@ export default class HomeView extends Vue {
     this.graph.on('mouseup', (param: any) => {
       let self:any = this
       // console.log("up")
-      // console.log(self.fromObj, self.toObj)
+      // console.log(self.fromObj.data["category"], self.toObj)
       if (param.dataType == "node") {
         self.delNodeVis = true
         self.toObj = param
-        if (self.fromObj != null && self.fromObj.dataType == "node" && self.fromObj.name != self.toObj.name) {
+        if (self.fromObj != null && self.fromObj.dataType == "node" && self.fromObj.name != self.toObj.name && self.toObj.data["category"] != self.fromObj.data["category"]) {
           let cnt:number = 0
           self.links.push(
               {
                 source: self.fromObj.name,
                 target: self.toObj.name,
                 lineStyle: {
+                  color: self.fromObj.data["category"] === "Resource" ? "rgb(0, 0, 128)" : "rgb(128, 0, 0)",
                   curveness: 0.0
                 }
               }
@@ -275,7 +298,7 @@ export default class HomeView extends Vue {
                 self.links[i].lineStyle["curveness"] = -0.4 + 0.8 * (tmp / (cnt - 1))
                 self.links[i].lineStyle["curveness"] *= flag
                 tmp += 1
-                console.log(self.links[i].lineStyle["curveness"])
+                // console.log(self.links[i].lineStyle["curveness"])
               }
             }
           }
@@ -472,9 +495,10 @@ export default class HomeView extends Vue {
     this.curObj = null
     this.graph.setOption(this.option)
   }
-  submitForm = (formEl: any) => {
+  submitForm = (formEl: any, e:any) => {
     // console.log(formEl)
     //const tab: any = this.$refs[formEl]
+    this.myBlur(e)
     formEl.validate((valid: any) => {
       if (valid) {
         for (let i in this.nodes) {
@@ -524,6 +548,14 @@ export default class HomeView extends Vue {
     name: [{ validator: this.validateName, trigger: 'blur' }],
     value: [{ validator: this.validateValue, trigger: 'blur' }],
   })
+  myBlur(event: any) {
+    let tar = event.target
+    while (tar.nodeName !== "BUTTON") {
+      tar = tar.parentNode
+    }
+    tar.blur()
+  }
+
 }
 </script>
 
@@ -538,7 +570,7 @@ export default class HomeView extends Vue {
 }
 .graph {
   background-color: #ffffff;
-  box-shadow:5px 3px 8px -2px #000;
+
   background-image: linear-gradient(90deg, rgba(50, 0, 0, 0.15) 3%, rgba(0, 0, 0, 0) 3%),
   linear-gradient(360deg, rgba(50, 0, 0, 0.15) 3%, rgba(0, 0, 0, 0) 3%);
   background-size: 20px 20px;
@@ -601,4 +633,13 @@ export default class HomeView extends Vue {
   background-color: rgb(3, 125, 243);;
   color: white;
 }
+
+.chat {
+  margin-top: 5px;
+  border-radius: 15px;
+  border: solid azure;
+  height: 840px;
+  box-shadow:0px 0px 8px -2px #000;
+}
+
 </style>
